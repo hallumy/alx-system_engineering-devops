00cbc16fc1f97bf975d5852390a951e9a8c53d8e
Ahlam Abdulkhalek <alhamny@gmail.com>
2023-01-19 22:54:09 +0300

    Created a file in tmp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Creating a file in /tmp
+
+file { 'school':
+ensure  => 'present',
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
+}
