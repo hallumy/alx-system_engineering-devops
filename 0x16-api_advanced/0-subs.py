807ac56d2ba9fef429af51985b93ed8644f92084
Ahlam Abdulkhalek <alhamny@gmail.com>
2024-06-04 18:42:05 +0300

    Correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,11 @@ def number_of_subscribers(subreddit):
     """This function returns 0 if an invalid subreddit
     is given""" 
     subscribe = 0
-    headers = {'User-agent': 'Chrome/125.0.0.0'}
-    url = 'https://www.reddit.com/response/' + subreddit + '/about.json'
+    headers = {'User-agent': 'Google Chrome Version 125.0.0.0'}
+    url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-       	info = response.jason().get('info')
+       	info = response.jason().get('data')
 	if info:
 	   subscribe = info.get('subscriber')
 	return subscribe 
